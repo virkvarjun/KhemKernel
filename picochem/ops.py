429e86a3a888a97708d6cace5ab57c504bfebb33
Arjun V <virkvarjun@gmail.com>
2026-05-01 12:10:35 -0400

    Added GeLU

--- picochem/ops.py
+++ picochem/ops.py
@@ -16,3 +16,19 @@ def linear_backward(grad_y, cache):
     grad_b = grad_y.sum(axis=0)
     return grad_x, grad_W, grad_b
 
+# Here, let's work on the GeLU 
+GELU_CONST = np.sqrt(2.0 / np.pi) 
+def gelu_forward(x): 
+    inner = GELU_CONST * (x*0.044715 * x ** 3) 
+    tanh_inner = np.tanh(inner) 
+    y = 0.5 * x * (1 + tanh_inner) 
+    cache = (x, tanh_inner) 
+    return y, cache 
+def gelu_backward(grad_y, cache): 
+    x, tanh_inner = cache 
+    # Derivative of the GeLU with respect to x 
+    sech_sq = 1.0 - tanh_inner ** 2
+    d_inner = GELU_CONST * (1.0+3.0 * 0.04475 * x ** 2) 
+    d_gelu = 0.5 * (1.0 + tanh_inner) + 0.5 * x * sech_sq * d_inner 
+    grad_x = grad_y * d_gelu 
+    return (grad_x,) 
